dc0d229aef4be47332ed95874c9156e1c98044b7
Aleksa Bubanja <aleksa3b@gmail.com>
2022-01-02 20:00:48 +0100

    header finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Jobs from "./component/Jobs";
 import Search from "./component/Search";
+import HeaderImg from "./bg-header-desktop.svg";
 
 function App() {
   return (
     <main>
+      <header>
+        <img src={HeaderImg} alt="IMG" />
+      </header>
       <Search />
       <Jobs />
     </main>
