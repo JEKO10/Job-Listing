a7e23f22b71ac43ed29cba2c2866107c7f42149d
Aleksa Bubanja <aleksa3b@gmail.com>
2022-01-03 17:48:03 +0100

    css finished for tags

--- src/component/SingleJob.jsx
+++ src/component/SingleJob.jsx
@@ -2,31 +2,33 @@ import React from "react";
 
 function SingleJob({ job }) {
   return (
-    <div className="job">
-      <img src={job.logo} alt={job.company} />
+    <div className={`job ${job.featured ? "featured" : ""}`}>
       <div>
+        <img src={job.logo} alt={job.company} />
         <div>
-          <h1>{job.company}</h1>
-          <h1>Featured</h1>
-          <h1>New</h1>
-        </div>
-        <div>
-          <h1>{job.position}</h1>
-        </div>
-        <div>
-          <h1>{job.postedAt}</h1>
-          <h1>{job.contract}</h1>
-          <h1>{job.location}</h1>
+          <div>
+            <h1 id="name">{job.company}</h1>
+            {job.featured ? <h1 id="featured">Featured</h1> : ""}
+            {job.new ? <h1 id="new">New</h1> : ""}
+          </div>
+          <div>
+            <h1 id="position">{job.position}</h1>
+          </div>
+          <div>
+            <h1 id="posted">{job.postedAt}</h1>
+            <h1 id="contract">{job.contract}</h1>
+            <h1 id="location">{job.location}</h1>
+          </div>
         </div>
       </div>
       <div>
-        <h1>{job.role}</h1>
-        <h1>{job.level}</h1>
+        <h1 className="tag">{job.role}</h1>
+        <h1 className="tag">{job.level}</h1>
         {job.languages.map((lang) => {
-          return <h1>{lang}</h1>;
+          return <h1 className="tag">{lang}</h1>;
         })}
         {job.tools.map((tool) => {
-          return <h1>{tool}</h1>;
+          return <h1 className="tag">{tool}</h1>;
         })}
       </div>
     </div>
